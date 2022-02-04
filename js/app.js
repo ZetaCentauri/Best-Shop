d7d1da5cdddc351dc2f084ab50713de4bd5b2cd2
Joanna <joanna.szczerbowska@gmail.com>
2022-02-04 14:28:41 +0100

    Variables in js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// -----------------ZMIENNE-----------------//
+
+const productsInput = document.querySelector('.products-input')
+const ordersInput = document.querySelector('.orders-input');
+const selectInput = document.querySelector('.calc-form__select-input');
+const dropdownList = document.querySelector('.calc-form__dropdown-list');
+const selectBasic = document.querySelector('.dropdown-list__item.basic');
+const selectProfessional = document.querySelector('.dropdown-list__item.professional');
+const selectPremium = document.querySelector('.dropdown-list__item.premium');
+const accountingCheckbox = document.querySelector('.accounting-checkbox');
+const terminalCheckbox = document.querySelector('.terminal-checkbox');
+const productsOutput =  document.querySelector('.products-output');
+const ordersOutput =  document.querySelector('.orders-output');
+const packageOutput =  document.querySelector('.package-output');
+const accountingOutput =  document.querySelector('.accounting-output');
+const terminalOutput =  document.querySelector('.terminal-output');
+const productsCalculations = document.querySelector('.products-output .choice__value');
+const ordersCalculations = document.querySelector('.orders-output .choice__value');
+const chosenPackage = document.querySelector('.package-output .choice__value');
+const productsPrice = document.querySelector('.products-output .choice__price');
+const ordersPrice = document.querySelector('.orders-output .choice__price');
+const packagePrice =  document.querySelector('.package-output .choice__price');
+const accountingPrice = document.querySelector('.accounting-output .choice__price');
+const terminalPrice = document.querySelector('.terminal-output .choice__price');
+const totalPrice = document.querySelector('.total__price');
+
+
